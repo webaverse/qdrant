74001777ab23151ab8b495b939e3b3238fc6033b
Avaer Kazmer <a@modules.io>
2023-01-21 09:36:00 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-# sudo sh -c "ulimit -n 65535"
-sudo docker run -p 6333:6333 \
-  -v $(pwd)/data/storage:/qdrant/storage \
-  -v $(pwd)/data/custom_config.yaml:/qdrant/config/production.yaml \
-  --ulimit nofile=65535:65535 \
-  qdrant/qdrant
+./target/release/qdrant
